70088786f18ab23c3e92944592794c57337b54e2
Dempaman <32450932+Dempaman@users.noreply.github.com>
2018-04-26 15:22:37 +0200

    react added

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Login from './Login';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+  <div>
+    <Login />
+  </div>,
+  document.getElementById('root')
+  );
+registerServiceWorker();
